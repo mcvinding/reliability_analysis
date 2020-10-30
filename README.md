cc811dd3de284a42604a48bf31c9332b15ca67ee
mcvinding <mikkel.vinding@gmail.com>
2020-10-30 18:06:25 +0100

    towards calculaten Do and De

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # reliability_analysis
-Assessing reliability between measurements in neuroscience
+Assessing reliability between measurements in neuroscience.
+
+The intention is to provide a test for relaiability tests of continous neural time series in a way that is analogous to Krippendorff's Alpha. So far it is only work in progress and not tested if it works or is compatible.
--- alphaprime.m
+++ alphaprime.m
@@ -22,17 +22,21 @@ dat = [x; y];   % N observers vs M samples
 % y = rand(1,20);
 
 dat = [x; y];   % N observers vs M samples
-
-kriAlpha(dat, 'interval')
-
+tic
+alpha = kriAlpha(dat, 'interval')
+toc
 %% New method
+% TO DO:
+% * Option to specify resolution
+N = 100;        % Default in linspace
 
 %% Find joint probability function over time
+% Clean up this part. Naming is inconsitent and there are redundant parts.
 kern_res = [dt*1, range(dat(:))/20];
       
 % Get joint density function over time
-gridx1 = linspace(min(dat(:)),max(dat(:)));
-gridx2 = t;
+gridx1 = linspace(min(dat(:)),max(dat(:)), N);   % Data axis. Evenly spaces mased on max/min of data
+gridx2 = t;                                   % Time axis (copy from real data)
 [x1,x2] = meshgrid(gridx1, gridx2);
 x1 = x1(:);
 x2 = x2(:);
@@ -60,6 +64,35 @@ subplot(1,20,1:3); plot(pg,xi2); axis tight; hold on
 plot(pgsum,xi2)
 subplot(1,20,5:20); imagesc(gridx2,gridx1,dM); colorbar
 set(gca,'YDir','normal')
+
+%% Sandbox
+% Loop over repetitions
+
+delta = (x-y).^2;   % Error fun for interval data (Krippendorff, 2018)
+
+
+for u = 1:size(dat,2)
+    c = dat(1,u);
+    k = dat(2,u);
+    tu = t(u);
+    du = delta(u);
+    
+    % Find density of observed values
+    % c = 2
+    % k = 5
+    % u = 0.100
+
+    P_c = interp1(gridx1, pg, c, 'spline');
+    P_k = interp1(gridx1, pg, k, 'spline');
+
+    Puc = interp2(gridx2, gridx1, dM, tu, c, 'spline');
+    Puk = interp2(gridx2, gridx1, dM, tu, k, 'spline');
+
+    Do(u) = (Puc+Puk)*delta(u);
+    De(u) 
+
+end
+
 
 
     
