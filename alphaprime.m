94b66a45ed69e8f57a50d941d68301993d38b02b
mcvinding <mikkel.vinding@gmail.com>
2020-12-13 16:05:02 +0100

    test multiple methods

--- alphaprime.m
+++ alphaprime.m
@@ -19,29 +19,23 @@ title(aa)
 
 dat = [x; y];   % N observers vs M samples
 
-
-
 %% Test with kriAplha function
-% x = 1:20;
-% y = rand(1,20);
-
-% dat = [x; y];   % N observers vs M samples
 tic
 alpha1(aa) = kriAlpha(dat, 'interval')
 toc
 %% New method
 % TO DO:
 % * Option to specify resolution
-N = 200;        % Default in linspace
+N = 100;        % 100 = Default in linspace
 
-%% Find joint probability function over time
+%% Find joint probability function over time (
 % Clean up this part. Naming is inconsitent and there are redundant parts.
-kern_res = [dt*1, range(dat(:))/50];
+kern_res = [dt*1, range(dat(:))/100];
       
 % Get joint density function over time
 gridx1 = linspace(min(dat(:)),max(dat(:)), N);   % Data axis. Evenly spaces mased on max/min of data
-gridx2 = t;                                      % Time axis (copy from real data)
-[x1,x2] = meshgrid(gridx1, gridx2);
+tdim = t;                                      % Time axis (copy from real data)
+[x1,x2] = meshgrid(gridx1, tdim);
 x1 = x1(:);
 x2 = x2(:);
 xi = [x2 x1];
@@ -50,196 +44,205 @@ tmp = dat';
 yz = tmp(:);
 xz = repmat(t, 1, size(dat,1));
 Xz = [xz', yz];
-[dO, xi1, kn] =  ksdensity(Xz,xi,'Bandwidth', kern_res);
+[dO, xi1, kn] =  ksdensity(Xz,xi,'Bandwidth', kern_res, 'Function', 'pdf');
 
 % Get probabilities
-% po = dO/sum(dO);
-% pO = reshape(po,length(gridx2),length(gridx1))';
-
-dO = reshape(dO,length(gridx2),length(gridx1))';
+po = dO/sum(dO);
+pO = reshape(po,length(tdim),length(gridx1))';
+dO = reshape(dO,length(tdim),length(gridx1))';
 
 
 % Get marginal
-[dE, xi2] =  ksdensity(dat(:),gridx1,'Bandwidth', kern_res(2));
+[dE, xi2] =  ksdensity(dat(:),gridx1,'Bandwidth', kern_res(2),'Function', 'pdf');
 
-% pE = dE/(sum(dE));
-dE = sum(dO,2);
-totals = sum(dO,1);
+pE = dE/(sum(dE));
+% pE = sum(pO,2);
+dE2 = sum(dO,2);
+pE2 = dE2/sum(dE2);
+totals = sum(pO,1);
 
-% Plot joint 
-figure;
+% Plot density
+figure; 
 subplot(1,20,1:3); plot(dE,xi2); axis tight; hold on
-% plot(pgsum,xi2)
-subplot(1,20,5:20); imagesc(gridx2,gridx1,dO); colorbar
-set(gca,'YDir','normal')
+plot(dE2,xi2)
+subplot(1,20,5:20); imagesc(tdim,gridx1,dO); colorbar
+set(gca,'YDir','normal');
+title(['Dens. aa=', num2str(aa)])
+
+% Plot probability
+figure; 
+subplot(1,20,1:3); plot(pE,xi2); axis tight; hold on
+plot(pE2,xi2)
+subplot(1,20,5:20); imagesc(tdim,gridx1,pO); colorbar
+set(gca,'YDir','normal');
+title(['Prob. aa=', num2str(aa)])
 
 n__ = sum(sum(dO));              %length(dat(:));
 nu_ = sum(dO,1);                 %size(dat,1);
 
-% Calculate alpha
-for tt = 1:length(gridx2)
-    u = gridx2(tt);
+%% Calculate alpha
+% Density approach
+tic
+allvals = unique(dat);
+
+% nominator
+for tt = 1:length(tdim)
+    u = tdim(tt);
     
-    for ii = 1:length(gridx1)-1
-        c = gridx1(ii);
-        kvals = gridx1(ii+1:end);
+    for ii = 1:length(allvals)-1
+        c = allvals(ii);
+        kvals = allvals(ii+1:end);
+        deltas = (kvals-c).^2;
+    
+        nuc = interp2(tdim, gridx1, dO, u, c, 'linear');
+        nuk = interp2(tdim, gridx1, dO, u, kvals, 'linear');
+        Znucnuk(ii) = sum(nuc*nuk.*deltas);
+    end
+    Zu(tt) = sum(Znucnuk./(nu_(tt)));
+    Zu2(tt) = sum(Znucnuk);
+end
+
+% denominator
+for ii = 1:length(allvals)-1
+    c = allvals(ii);
+    kvals = allvals(ii+1:end);
+    deltas = (kvals-c).^2;
+%     n_c = interp1(gridx1, dE2, c, 'linear');
+%     n_k = interp1(gridx1, dE2, kvals, 'linear');
+
+    n_c = interp1(gridx1, dE2, c, 'linear');
+    n_k = interp1(gridx1, dE2, kvals, 'linear');
+
+    Zncnk(ii) = sum(n_c*n_k.*deltas);
+end
+
+Do = sum(Zu);
+Do2 = sum(Zu2);
+
+De = sum(Zncnk) % / (n__-1);
+De2 = sum(Zncnk) / (n__-1);
+
+
+alphaD(aa) = 1 - (Do/De);
+alphaD2(aa) = 1 - (Do2/De2);
+
+dt = toc;
+fprintf('Calculating Alphaprime took %.2f sec\n', dt)
+
+%% Probability approach
+tic
+nu_ = sum(pO,1);
+allvals = unique(dat);
+for tt = 1:length(tdim)
+    u = tdim(tt);
+    
+    for ii = 1:length(allvals)-1
+        c = allvals(ii);
+        kvals = allvals(ii+1:end);
         deltas = (kvals-c).^2;
     
         % nominator
-        nuc = dO(ii,tt);
-        nuk = dO(ii+1:end, tt);
-        Znucnuk(ii) = sum(nuc*nuk.*deltas');
+        nuc = interp2(tdim, gridx1, pO, u, c, 'linear');
+        nuk = interp2(tdim, gridx1, pO, u, kvals, 'linear');
+        Znucnuk(ii) = sum(nuc*nuk.*deltas);
 
         % denominator
-        n_c = dE(ii);
-        n_k = dE(ii+1:end);
-        Zncnk(ii) = sum(n_c*n_k.*deltas');
+        n_c = interp1(gridx1, pE2, c, 'linear');
+        n_k = interp1(gridx1, pE2, kvals, 'linear');
+        Zncnk(ii) = sum(n_c*n_k.*deltas);
+        
+    end
+    Zu(tt) = sum(Znucnuk);
+end
+
+for tt = 1:length(tdim)
+    Zu(tt) = sum(Znucnuk./(nu_(tt)));
+end
+    
+    
+Do = sum(Zu);
+De = sum(Zncnk);
+
+alphaP(aa) = 1 - (Do/De);
+
+% timer
+dt = toc;
+fprintf('Calculating Alphaprime took %.2f sec\n', dt)
+
+% end
+
+%% hist3 approach (absolute values)
+% tic
+allvals = unique(dat);
+tdim = 1:size(dat,2);
+
+res = length(allvals); % data resolution
+
+X = dat(:);
+Y = repmat(1:size(dat,2),size(dat,1),1);
+Y = Y(:);
+
+XY = [X, Y];    %Data vale, time bin
+
+% ll = linspace(min(dat(:)), max(dat(:)), res);
+ll = allvals;
+dO2 = hist3(XY, {ll, 1:size(dat,2)});
+
+dE2 = sum(dO2,2);
+totals2 = sum(dO2,1);
+
+% figure;
+% subplot(1,20,1:3); plot(dE2,ll); axis tight; hold on
+% % plot(pgsum,xi2)
+% subplot(1,20,5:20); imagesc(1:size(dat,2),ll,dO2); colorbar
+% set(gca,'YDir','normal')
+
+% P_vals = interp1(grid1, dE, allVals, 'spline');
+
+%
+n__ = sum(sum(dO2));              %length(dat(:));
+nu_ = sum(dO2,1);                 %size(dat,1);
+
+for tt = 1:length(tdim)
+%     u = gridx2(tt);
+    
+    for ii = 1:length(allvals)-1
+        c = allvals(ii);
+        kvals = allvals(ii+1:end);
+        deltas = (kvals-c).^2;
+    
+        % nominator
+        nuc = dO2(ii,tt);
+        nuk = dO2(ii+1:end, tt);
+        Znucnuk(ii) = sum(nuc*nuk.*deltas);
+
+        % denominator
+        n_c = dE2(ii);
+        n_k = dE2(ii+1:end);
+        Zncnk(ii) = sum(n_c*n_k.*deltas);
         
     end
     Zu(tt) = sum(Znucnuk./(nu_(tt)-1));
 end
+toc
 
 Do = sum(Zu);
 De = sum(Zncnk) / (n__-1);
 
-alphaX(aa) = 1 - (Do/De)
-
-end
-
-%%
-figure;
-scatter(alpha1,alphaX)
-
-%% Coinciedse matrix approach
-% Reshape the datamatric M (for M > 2)
-allVals=unique(dat(:));
-allVals=allVals(isfinite(allVals));
-
-% for M = 1:size(dat,1)
-%     
-%     
-% end
-
-% Calculate dOck
-kern_res = [range(allVals)/50, range(allVals)/50];
-grid1 = linspace(min(allVals),max(allVals), N);  % Use this as approximation or all values as is now )mioght be inefficient)?
-
-[x1,x2] = meshgrid(grid1, grid1);
-x1 = x1(:);
-x2 = x2(:);
-xi = [x2 x1];
-Xz = [dat(1,:)', dat(2,:)'];
-
-% Confusion density
-[ox, ~, kn] =  ksdensity(Xz,xi, 'PlotFcn','contour','Bandwidth', kern_res);
-% ksdensity(Xz,xi, 'PlotFcn','contour','Bandwidth', kern_res)
-po = ox/sum(ox);
-pO = reshape(po,length(grid1),length(grid1))';
-
-% imagesc(grid1,grid1,dX)
-
-% Get marginal
-[e, ~] =  ksdensity(allVals,grid1,'Bandwidth', min(kern_res(2)));
-[e2, ~] =  ksdensity(allVals,grid2,'Bandwidth', min(kern_res(2)));
-
-de = e/(sum(e));
-pgsum1 = sum(pO,1);
-pgsum2 = sum(pO,2);
-
-% Plot joint 
-figure;
-subplot(5,5,[1 6 11, 16]); plot(pgsum1,grid1); axis tight; set(gca,'YDir','reverse')
-subplot(5,5,[2:5 7:10, 12:15, 17:20]); imagesc(grid1,gridx1,pO);
-subplot(5,5,22:25); plot(grid1,pgsum2);axis tight;
-
-tic
-P_vals = interp1(grid1, dE, allVals, 'spline');
-Desum = zeros(length(allVals)-ii,1);
-Dosum = zeros(length(allVals)-ii,1);
-for ii = 1:length(allVals)-1
-    c = allVals(ii);
-
-%     De = zeros(length(allVals)-i-1,1);
-%     Do = zeros(length(allVals)-i-1,1);
-
-    kvals = allVals(ii+1:end);
-    delta = (kvals-c).^2;
-    Pck = interp2(grid1, grid1, pO, c, kvals, 'spline');
-    dOC(ii) = sum(Pck.*delta);
-    dEC(ii) = sum((P_vals(ii).*P_vals(ii+1:end)).*delta);
-end
-
-do = sum(dOC)
-de = sum(dEC)
-toc
-
-alpha2 = 1-sum(do)/sum(de);
-
-
-
-
-%%
-    P_k = 
-    
-    for j = ii+1:length(allVals)
-        fprintf('i=%i, j=%i\n', ii, j)
-        c = allVals(ii);
-        k = allVals(j);
-%         P_k = interp1(grid1, dE, k, 'spline');
-        Pck = interp2(grid1, grid1, pO, c, kvals, 'spline');
-        
-%     Puk = interp2(gridx2, gridx1, dM, tu, k, 'spline');
-    
-        De(j) = (P_c+P_k)*(c-k).^2;
-        Do(j) = Pck*(c-k).^2;
-
-%         tmp = 
-%         do=do+coinMatr(c,k)*(allVals(c)-allVals(k)).^2;
-%         de=de+nc(c)*nc(k)*(allVals(c)-allVals(k)).^2;
-    end
-    Desum(ii) = sum(De);
-    Dosum(ii) = sum(Do);
-end
-toc
-
-nc=sum(pO,1);
-n=sum(nc);
-
-alpha = 1-sum(Dosum)/sum(Desum);
-
-
-%% Loop over repetitions
-
-delta = (x-y).^2;   % Error fun for interval data (Krippendorff, 2018)
-
-
-for u = 1:size(dat,2)
-    c = dat(1,u);
-    k = dat(2,u);
-    tu = t(u);
-    du = delta(u);
-    
-    % Find density of observed values
-    % c = 2
-    % k = 5
-    % u = 0.100
-
-    P_c = interp1(gridx1, pE, c, 'spline');
-    P_k = interp1(gridx1, pE, k, 'spline');
-
-    Puc = interp2(gridx2, gridx1, pO, tu, c, 'spline');
-    Puk = interp2(gridx2, gridx1, pO, tu, k, 'spline');
-
-    Do(u) = (Puc+Puk)*delta(u);
-    De(u) 
+alphaH(aa) = 1 - (Do/De);
 
 end
 
 
-%%
+disp('done')
+
+
+% %%
+% figure;
+% subplot(1,3,1); scatter(alpha1,alphaP)
+% subplot(1,3,2); scatter(alpha1,alphaX2)
+% subplot(1,3,3);scatter(alphaP,alphaX2)
 
 
 
-    
-       
-       
+%END       
