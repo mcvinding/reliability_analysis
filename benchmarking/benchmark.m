44db56045fa8bbe83defe21715d1eb98dab622f4
mcvinding <mikkel.vinding@gmail.com>
2024-08-13 17:47:26 +0200

    unknown changes

--- benchmarking/benchmark.m
+++ benchmarking/benchmark.m
@@ -1,21 +1,29 @@
-% Benchmarking errors
+% Benchmarking: test how values of alpha are realted to %-agreement for
+% ordinal, 
+
+
+
 addpath('/home/mikkel/reliability_analysis')
 
-%% Ordinal/binary data
+%% Settings
+outdir = '/home/mikkel/reliability_analysis/benchmarking/output';
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Nominal/binary data
 N = 1000;
 Nperm = 100;
-err = 0.0:0.005:0.5;  %  Percent disagreement
+errPct = 0.0:0.01:0.5;  %  Percent disagreement
 
-%% One-sample case
-truedat = [ones(1,N*0.2), zeros(1,N*0.82)];
-alph_ord1 = zeros(Nperm, length(err));
+truedat = [ones(1,N*0.05), zeros(1,N*0.95)];
 
-for ee = 1:length(err)
+%% Nominal: One-sample
+alph_nom1 = zeros(Nperm, length(errPct));
+
+for ee = 1:length(errPct)
     for ss = 1:Nperm
         errdat = truedat;
         rx = randperm(N);
-        slct = rx(1:round(N*err(ee)));
+        slct = rx(1:round(N*errPct(ee)));
     
         for ii = 1:length(slct)
             if errdat(slct(ii)) == 1
@@ -27,32 +35,27 @@ for ee = 1:length(err)
     
         dat = [truedat; errdat];
         
-        alph_ord1(ss,ee) = kripAlpha(dat, 'ordinal');
+        alph_nom1(ss,ee) = kripAlpha(dat, 'nominal');
     end
 end
+disp('done')
 
 % figure(1)
 % scatter(err, alph1, 'b'); hold on
 % plot(err, alph1, 'b')
 
-figure(1); hold on
-errorbar(err, mean(alph_ord1), std(alph_ord1), 'b', 'MarkerSize',12);
-yline()
+%% Nominal: Independent samples
+alph_nom2 = zeros(Nperm, length(errPct));
 
-set(gcf, 'Position', [500, 500, 700, 500])
-
-%% Independent samples
-alph_ord2 = zeros(Nperm, length(err));
-
-for ee = 1:length(err)
+for ee = 1:length(errPct)
     for ss = 1:Nperm
         dat1 = truedat;
         dat2 = truedat;
     
         rx1 = randperm(N);
         rx2 = randperm(N);
-        slct1 = rx1(1:round(N*err(ee)));
-        slct2 = rx2(1:round(N*err(ee)));
+        slct1 = rx1(1:round(N*errPct(ee)));
+        slct2 = rx2(1:round(N*errPct(ee)));
     
         for ii = 1:length(slct1)
             if dat1(slct1(ii)) == 1
@@ -71,26 +74,100 @@ for ee = 1:length(err)
         end
         dat = [dat1; dat2];
         
-        alph_ord2(ss, ee) = kripAlpha(dat, 'ordinal');
+        alph_nom2(ss, ee) = kripAlpha(dat, 'nominal');
     end
 end
+disp('done')
 
 % scatter(err, alph2, 'r'); hold on
 % plot(err, alph2, 'r')
 
+%% Plot
 figure(1); hold on
-errorbar(err, mean(alph_ord2), std(alph_ord2), 'r', 'MarkerSize',12);
+errorbar(errPct, mean(alph_nom1), std(alph_nom1), 'b-', 'MarkerSize',12);
+errorbar(errPct, mean(alph_nom2), std(alph_nom2), 'r-', 'MarkerSize',12);
+set(gcf, 'Position', [500, 500, 700, 500])
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Ordinal data
+N = 1000;
+Nperm = 100;
+errPct = 0.0:0.02:1;  %  Percent disagreement
+ordVals = [1,2,3,4,5,6,7,8,9,10];
 
+truedat = repmat(ordVals, 1, N/length(ordVals));
+truedat = truedat(randperm(N));
+
+%% Ordinal: One-sample
+alph_ord1 = zeros(Nperm, length(errPct));
+for ee = 1:length(errPct)
+    for ss = 1:Nperm
+        errdat = truedat;
+        rx = randperm(N);
+        slct = rx(1:round(N*errPct(ee)));
+
+        for ii = 1:length(slct)
+            tmp = ordVals(~(ordVals==errdat(slct(ii))));
+            tmp = tmp(randperm(length(tmp)));
+            errdat(slct(ii)) = tmp(1);
+        end
+
+        dat = [truedat; errdat];
+        
+        alph_ord1(ss,ee) = kripAlpha(dat, 'ordinal');
+    end
+end
+disp('done')
+
+%% Ordinal: independent sample
+alph_ord2 = zeros(Nperm, length(errPct));
+for ee = 1:length(errPct)
+    for ss = 1:Nperm
+        dat1 = truedat;
+        dat2 = truedat;
+
+        rx1 = randperm(N);
+        rx2 = randperm(N);
+
+        slct1 = rx1(1:round(N*errPct(ee)));
+        slct2 = rx2(1:round(N*errPct(ee)));
+
+        for ii = 1:length(slct1)
+            tmp = ordVals(~(ordVals==dat1(slct1(ii))));
+            tmp = tmp(randperm(length(tmp)));
+            dat1(slct1(ii)) = tmp(1);
+        end
+
+        for ii = 1:length(slct2)
+            tmp = ordVals(~(ordVals==dat2(slct2(ii))));
+            tmp = tmp(randperm(length(tmp)));
+            dat2(slct2(ii)) = tmp(1);
+        end
+
+        dat = [dat1; dat2];
+        
+        alph_ord2(ss,ee) = kripAlpha(dat, 'ordinal');
+    end
+end
+disp('done')
+
+%% Plot
+
+figure(2); hold on
+errorbar(errPct, mean(alph_ord1), std(alph_ord1), 'b-', 'MarkerSize',12);
+errorbar(errPct, mean(alph_ord2), std(alph_ord2), 'r-', 'MarkerSize',12);
+set(gcf, 'Position', [500, 500, 700, 500])
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Interval data
-
 %Generate two time-series : settings
 fs       = 1000;             % Sampling frequency (samples per second) 
 dt       = 1/fs;             % seconds per sample 
 StopTime = 1;                % seconds 
 t        = (dt:dt:StopTime); % seconds 
 Freq     = 5;                % Sine wave frequency (hertz) 
-err      = 0:0.1:2;          % Noise scaling
+err   = 0:0.1:2;             % Noise scaling for noise added
+
 Nperm    = 100;
 
 truedat = sin(2*pi*Freq*t);
@@ -101,16 +178,16 @@ alph_int1 = zeros(Nperm, length(err));
 
 for ee = 1:length(err)
     for ss = 1:Nperm
-        errdat = truedat + randn(1,length(t))*err(ee);
-    
+        errdatAdd = truedat + randn(1,length(t))*err(ee);
+        errdatRep = truedat + randn(1,length(t))*err(ee);
+
         dat = [truedat; errdat];
         
         alph_int1(ss,ee) = kripAlpha(dat, 'interval');
     end
 end
+disp('done')
 
-figure(2); hold on
-errorbar(err, mean(alph_int1), std(alph_int1), 'b', 'MarkerSize',12);
 
 %% Two-sample: interval + added noise
 alph_int2 = zeros(Nperm, length(err));
@@ -127,7 +204,9 @@ for ee = 1:length(err)
 end
 disp('done')
 
-figure(2); hold on
+%% Plot
+figure(3); hold on
+errorbar(err, mean(alph_int1), std(alph_int1), 'b', 'MarkerSize',12);
 errorbar(err, mean(alph_int2), std(alph_int2), 'r', 'MarkerSize',12);
 
 %% One-sample: interval + replacement noise (no scaling)
--- example1.m
+++ example1.m
@@ -1,4 +1,4 @@
-% Use example data frame from Krippendorff
+% Use example dataframe from Krippendorff (ref)
 
 A = [1 2 3 3 2 1 4 1 2 nan nan nan];
 B = [1 2 3 3 2 2 4 1 2 5 nan 3];
@@ -8,8 +8,8 @@ D = [1 2 3 3 2 4 4 1 2 5 1 nan];
 dat = [A; B; C; D];
 
 %% Get Krippendorff's Alpha
-alpha_int = kripAlpha(dat, 'interval'); disp('done')  % Assuming interval data
-alpha_ord = kripAlpha(dat, 'ordinal'); disp('done')   % Assuming ordinal data
+alpha_int = kripAlpha(dat, 'interval');  % Assuming interval data
+alpha_ord = kripAlpha(dat, 'ordinal');   % Assuming ordinal data
 
 %% Approximation method (only interval and large datasets)
 % Does not really make sense for this data.
